b6687bf08d33dd486e796017dd48a11f3aa23b11
MAYKJOEL <joel.14795@gmail.com>
2023-11-14 19:47:00 +0100

    primer commit

--- Simulación Challenge/checkpoint.py
+++ Simulación Challenge/checkpoint.py
@@ -12,7 +12,25 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    #numero = int(numero)
+    if str != type(numero):
+        if int == type(numero):
+            if numero < 0:
+                 print('Nulo')
+            elif numero == 0:
+                 print('Nulo')
+            elif numero > 0:
+                fact = 1
+                for i in range(numero+1):
+                    if i > 0:
+                        fact *= i
+                return fact
+                
+        elif float == type(numero):
+            print('Nulo')
+    elif str == type(numero):
+        print('Nulo') 
+
 
 def EsPrimo(valor):
     '''
@@ -26,25 +44,36 @@ def EsPrimo(valor):
         EsPrimo(8) debe retornar False
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    valor1 = valor
+    if str != type(valor):
+        if int == type(valor):
+            if valor < 1:
+                 print('Nulo')
+            elif valor == 1:
+                 print(1)
+            elif valor > 1:
+                Espri = 0
+                for i in range(valor):
+                    if i >= 2:
+                        if valor1%(i) == 0:
+                            Espri += 1
+                if Espri == 0:
+                    return True
+                else:
+                    return False
+                
+        elif float == type(valor):
+            print('Nulo')
+    elif str == type(valor):
+        print('Nulo')
     
-def ClaseAnimal(especie, color):
-    '''
-    Esta función devuelve un objeto instanciado de la clase Animal, 
-    la cual debe tener los siguientes atributos:
-        Edad    (Un valor de tipo de dato entero, que debe inicializarse en cero)
-        Especie (Un valor de tipo de dato string)
-        Color   (Un valor de tipo de dato string)
-    y debe tener el siguiente método:
-        CumplirAnios  (este método debe sumar uno al atributo Edad y debe devolver ese valor)
-    Recibe dos argumento:
-        especie: Dato que se asignará al atributo Especie del objeto de la clase Animal
-        color: Dato que se asignará al atributo Color del objeto de la clase Animal
-    Ej:
-        a = ClaseAnimal('perro','blanco')
-        a.CumpliAnios() -> debe devolver 1
-        a.CumpliAnios() -> debe devolver 2
-        a.CumpliAnios() -> debe devolver 3
-    '''
-    #Tu código aca:
-    return 'Funcion incompleta'
+class Animal:
+    def __init__(self,especie,color):
+        self.edad = 0
+        self.especie = especie
+        self.color = color
+    def CumplirAnios(self):
+        self.edad += 1
+        return self.edad
+a = Animal('Perro', 'Blanco')
+
